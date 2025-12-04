ef8edb59c6a669d2297009e90a0ec4b0bf4326ba
lucensolen <lucensolen.studio@gmail.com>
2025-12-04 13:10:45 +0000

    Update app.js

--- core/app.js
+++ core/app.js
@@ -6,7 +6,7 @@ ________________________________ */
 
 const LucenOS = {
   settings: {
-    footerMode: "static", // interactive | reveal | static
+    footerMode: "interactive", // interactive | reveal | static
     footerHideThreshold: 80,
     theme: "rich-neutral",
 
